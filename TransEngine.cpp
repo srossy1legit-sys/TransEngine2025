3f4223df422154506e734f4d96f29d5996ddb94b
srossy1legit-sys <srossy1legit@gmail.com>
2025-12-22 20:36:43 -0400

    Add files via upload

---
+++ TransEngine.cpp
@@ -0,0 +1,36 @@
+﻿// TransEngine.cpp : Defines the entry point for the application.
+//
+//Foreign Consoles "Xbox, PlayStation, Nintendo Switch, and Nintendo Switch 2"
+#include "Consoles/Xbox/xbox.h"
+#include "Consoles/PS5/ps5.h"
+#include "Consoles/PS4/ps4.h"
+#include "Consoles/Nintendo Switch 2/nintendoswitch2.h"
+#include "Consoles/Nintendo Switch/nintendoswitch.h"
+// --------------------------------------
+// Steam Products
+#include "Consoles/Steam/steamcontroller.h"
+#include "Consoles/Steam/steamdeck.h"
+#include "Consoles/Steam/steammachine.h"
+//--------------------------------------
+//Core
+#include "TransEngine.h"
+#include "Display/SDL3/SDL.h"
+#include "Display/OpenGL/glad.h"
+#include "Display/OpenGL/glfw3.h"
+#include "Display/OpenGL/glfw3native.h"
+//--------------------------------------
+//
+
+using namespace camera
+
+
+using namespace window
+
+
+using namespace 
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ TransEngine.h
@@ -0,0 +1,13 @@
+﻿// TransEngine.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+
+
+namespace TransEngineCompilation {
+	class OPENWINDOW
+}
